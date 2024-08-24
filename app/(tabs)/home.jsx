65627a5e827b02b4fa7f4aafb98ad4e61d1dc561
Bishr Mubarak <bishrmubaraq@gmail.com>
2024-08-24 18:30:49 +0530

    Implement search functionality and profile section

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -17,9 +17,12 @@ import EmptyState from "../../components/EmptyState";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
+
+  const { user } = useGlobalContext();
 
   const { data: posts, refetch, isLoading } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
@@ -44,7 +47,7 @@ const Home = () => {
                   Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Bishr Mubaraq
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5 ">
